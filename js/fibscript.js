57dc2cbcf47759456cb206664a2e1dcde0395dfa
stephsiler <36432333+stephsiler@users.noreply.github.com>
2019-11-25 10:15:21 -0500

    ed

---
+++ js/fibscript.js
@@ -0,0 +1,13 @@
+/* fibscript.js v0.3
+   Fill In Blank helper scripts 
+   original version by Chris Maverick 
+   11-24-19 */
+
+
+function fibUpdate(mySelectId) {
+    // sets a select's color based on the value sent to it
+
+    var fibSel = document.getElementById(mySelectId);
+    var answercolor = fibSel.options[fibSel.selectedIndex].value;
+    fibSel.style.backgroundColor = answercolor;
+}
